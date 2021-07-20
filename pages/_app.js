2c9d18515eeabf7b55c9d2554da309a61036de85
shenghann <shenghan@gmail.com>
2021-07-20 14:43:18 +0800

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,27 @@ import "../styles/global.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 library.add(faCaretUp);
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
   return <Component {...pageProps} />;
 }
 
