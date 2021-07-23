049d1da94390ac2f5654857e7b9c55d72ea2c13a
shenghann <shenghan@gmail.com>
2021-07-23 16:17:26 +0800

    add file copy plugin to get serverless func to work

--- pages/api/refresh.js
+++ pages/api/refresh.js
@@ -1,12 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getAllData } from "../../lib/data";
 
-// export default async function refresh(req, res) {
-//   const allData = await getAllData();
-//   res.status(200).json(allData);
-// }
-
-module.exports = (req, res) => {
-  const allData = getAllData();
+export default async function refresh(req, res) {
+  const allData = await getAllData();
   res.status(200).json(allData);
-};
+}
