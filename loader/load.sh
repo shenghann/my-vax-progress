f344fc73ec672f825988415e719772db39b1743c
shenghan <shenghan@gmail.com>
2021-08-17 08:58:18 +0800

    load script now checks for repo changes

--- loader/load.sh
+++ loader/load.sh
@@ -1,20 +1,37 @@
-CITF_LOCAL_REPO=/home/scadmin/citf-public
-VAXAPP_PATH=/home/scadmin/vaxapp/loader
-PYTHON_PATH=/home/scadmin/python3/bin/python3.6
+CITF_LOCAL_REPO=/home/shadmin/citf-public
+VAXAPP_PATH=/home/shadmin/vaxapp/loader
+PYTHON_PATH=/home/shadmin/python396/bin/python3.9
 now=$(date)
+echo "*******************************************"
 echo "[START RUN]: $now"
 
-echo "[INFO]    Git pull latest data from CITF public"
+echo "[INFO]    Checking CITF public..."
 cd $CITF_LOCAL_REPO
-git pull
+git fetch
+HEADHASH=$(git rev-parse HEAD)
+REMOTEHASH=$(git rev-parse main@{upstream})
+echo "[INFO]    Local:  $HEADHASH"
+echo "[INFO]    Remote: $REMOTEHASH"
 
-echo "[INFO]    Run loader script.."
-$PYTHON_PATH "$VAXAPP_PATH/scriptv2.py"
 
-echo "[INFO]    Commit data.json and git push to master branch.."
-cd $VAXAPP_PATH
-git add ../data/data.json
-git commit -m "citf update for 28 Jul"
-git push
+if [ "$HEADHASH" != "$REMOTEHASH" ]
+then
+    echo "[INFO]   New changes in CITF remote. Pulling latest"
+    git pull
 
-echo "[INFO]    Complete!"
+    echo "[INFO]    Run loader script.."
+    $PYTHON_PATH "$VAXAPP_PATH/scriptv2.py"
+
+    echo "[INFO]    Commit data.json and git push to master branch.."
+    cd $VAXAPP_PATH
+    git add ../data/data2.json
+    git commit -m "citf update for 28 Jul"
+    git push
+
+    echo "[INFO]    Complete!"
+
+else
+    echo "[INFO]   No new changes. Exiting..."
+    exit 0
+fi
+
